691bc48af66562cc1d4495d013ab9c51a8a017e1
dlsakbo <56906001+dlsakbo@users.noreply.github.com>
2020-07-07 09:52:05 +0530

    Revert "."
    This reverts commit bd07ccdb437b4d8f6bc14e129e7ea1421e55f913, reversing
    changes made to f4229e173a6dd6a6be7f1bb8752933e0ddac1b01.

--- RoomOffers/assets/js/main.js
+++ RoomOffers/assets/js/main.js
@@ -1,30 +1,9 @@
 let header = document.querySelector('.header');
 let hamburgermenu = document.querySelector('.hamburger-menu');
 
-
-window.addEventListener('scroll',function(){
-	let windowPosition=window.scrollY<0 ;
-	header.classList.toggle('active',window.scrollY>0)
-});
-
-
 hamburgermenu.addEventListener("click", function(){
  header.classList.toggle('menu-open');
 });
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
 
 (function($) {
 
