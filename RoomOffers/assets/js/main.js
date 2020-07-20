5eb4b6df82a987e1ed743197cd68254c0c7d5d2e
Kavindu Prabath <64495252+Kavprabath@users.noreply.github.com>
2020-07-20 13:56:48 +0530

    .

--- RoomOffers/assets/js/main.js
+++ RoomOffers/assets/js/main.js
@@ -25,7 +25,15 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
-
+$("button").on("click", function() {
+  var el = $(this);
+  if (el.text() == el.data("text-swap")) {
+    el.text(el.data("text-original"));
+  } else {
+    el.data("text-original", el.text());
+    el.text(el.data("text-swap"));
+  }
+});
 (function($) {
 
 	skel.breakpoints({
